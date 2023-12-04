32eda3b3c008bfc1f0b38a88c3c97c57c08bbe77
albertoCC05 <alberto.cst05@gmail.com>
2023-12-04 12:55:00 +0100

    limites superiores y movimiento

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // variables
+
+    private float horizontalInput;
+    private float verticalInput;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    private float verticalBounds = 3.83f;
+    private float horizontalBounds = 7.9f;
+    
+
+    //funciones
+
+    private void PlayerInBounds()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.z > verticalBounds)
+        {
+            pos.z = verticalBounds;
+        }
+        if (pos.z < -verticalBounds)
+        {
+            pos.z = -verticalBounds;
+        }
+
+        transform.position = pos;
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+    }
+    private void Rotation()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * horizontalInput);
+    }
+
+
+    //start/ update
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        MoveForward();
+        Rotation();
+        PlayerInBounds();
+
+
+    }
+
+
+
+}
